789d93750d483a0f47c0d8c4291f3efb6b26c7b7
siharshm <67157332+siharshm@users.noreply.github.com>
2025-02-26 15:21:43 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -9,11 +9,11 @@ menuBar.onclick = () =>{
 
 /*===== scroll section and sticky navbar===*/
 
-window.onscroll = () =>{
-    let header = document.querySelector('header');
+// window.onscroll = () =>{
+//     let header = document.querySelector('header');
 
-    header.classList.toggle('sticky', window.scrollY >100);
-}
+//     header.classList.toggle('sticky', window.scrollY >100);
+// }
 
 /*=== swiper js ===*/
 
@@ -34,3 +34,68 @@ var swiper = new Swiper(".myHome", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+  document.querySelectorAll('.faq-question').forEach(item => {
+    item.addEventListener('click', function() {
+        const answer = this.nextElementSibling;
+        answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
+    });
+});
+
+
+//   const services = [
+//     {
+//         title: "Vehicle registration",
+//         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ipsam eligendi iste repudiandae provident ab, corporis illo cupiditate non repellendus.",
+//         image: "./img/services/reg.jpg", 
+//         link: "#"
+//     },
+//     {
+//         title: "Free first three service",
+//         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ipsam eligendi iste repudiandae provident ab, corporis illo cupiditate non repellendus.",
+//         image: "./img/services/service.jpg",
+//         link: "#"
+//     },
+//     {
+//       title: "Original Japan warranty",
+//       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ipsam eligendi iste repudiandae provident ab, corporis illo cupiditate non repellendus.",
+//       image: "./img/services/warranty-image.jpg",
+//       link: "#"
+//   },
+//   {
+//     title: "After sales maintaining free advice",
+//     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ipsam eligendi iste repudiandae provident ab, corporis illo cupiditate non repellendus.",
+//     image: "./img/services/advice.jpg",
+//     link: "#"
+// },
+// {
+//   title: "leasing facilities",
+//   description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ipsam eligendi iste repudiandae provident ab, corporis illo cupiditate non repellendus.",
+//   image: "./img/services/leasing.jpg",
+//   link: "#"
+// },
+// {
+//   title: "Clearing and forwarding",
+//   description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ipsam eligendi iste repudiandae provident ab, corporis illo cupiditate non repellendus.",
+//   image: "./img/services/clearing-forwarding.jpg",
+//   link: "#"
+// }
+// ];
+
+// const servicesContainer = document.getElementById("services-list");
+
+// services.forEach(service => {
+//     const serviceHTML = `
+//         <div class="service-card">
+//             <img src="${service.image}" alt="${service.title}">
+//             <div class="service-content">
+//                 <h3>${service.title}</h3>
+//                 <p>${service.description}</p>
+//                 <a href="${service.link}" class="service-button">
+//                     More Detailed <i class='bx bx-right-arrow-alt'></i>
+//                 </a>
+//             </div>
+//         </div>
+//     `;
+//     servicesContainer.innerHTML += serviceHTML;
+// });
